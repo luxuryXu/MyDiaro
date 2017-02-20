ffa6b04aa32eb9b308d2246e3dacad5f3220620f
xuhaipeng <xuhaipeng@youbanban.com>
2017-02-20 15:27:04 +0800

    photo $ionicModal

--- www/js/controllers/mine.js
+++ www/js/controllers/mine.js
@@ -2,6 +2,15 @@
  * Created by Administrator on 2017/2/18.
  */
 angular.module('starter.controllers')
-.controller('MineCtrl' , function(){
-
+.controller('MineCtrl' , function($scope,$ionicModal,$rootScope){
+  $scope.showHeadList = function(){
+    var scope = $scope.$new();
+    $ionicModal.fromTemplateUrl('templates/photo.html',{
+      scope : scope,
+      animation : 'slide-in-up'
+    }).then(function(modal){
+      $scope.avatarModal = modal;
+      $scope.avatarModal.show();
+    });
+  };
 });
