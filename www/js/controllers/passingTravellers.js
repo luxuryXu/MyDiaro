b65da2ba25205534a36713fdaddf6b330ed9c325
xuhaipeng <xuhaipeng@youbanban.com>
2017-03-08 23:15:09 +0800

    passing-travellers

--- www/js/controllers/passingTravellers.js
+++ www/js/controllers/passingTravellers.js
@@ -2,6 +2,13 @@
  * Created by Administrator on 2017/2/18.
  */
 angular.module('starter.controllers')
-.controller('PassingTravellersCtrl' , function($scope,$state){
+.controller('PassingTravellersCtrl' , function($scope,$state,$ionicSlideBoxDelegate){
   $scope.name = $state.current.name;
+  $scope.index = 0;
+  $scope.select = function (index) {
+      $scope.index = index;
+  }
+  $scope.slideTo = function (index) {
+      $ionicSlideBoxDelegate.$getByHandle('passingSlide').slide(index,400);
+  }
 });
