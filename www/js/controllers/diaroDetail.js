a37a3dcf0db9397d1c37d9fec4953cfe750e6d16
xuhaipeng <xuhaipeng@youbanban.com>
2017-03-27 17:45:15 +0800

    diaro-detail

---
+++ www/js/controllers/diaroDetail.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 30613 on 2017/3/27.
+ */
+angular.module('starter.controllers')
+  .controller('DiaroCtrl' , function($scope){
+    $scope.time = '2017-03-25 16:13 周一';
+    $scope.collections = 7;
+    $scope.eyeNum = 5;
+    $scope.text = '呵呵呵呵呵呵呵或或或或或或或或呵呵呵呵呵呵呵或或或或或或或或或或或或或或坎坎坷坷扩啥核辐射的防护';
+  });
