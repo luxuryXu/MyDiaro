42b45fcfbe943ec2738ab41059b7b3c930794046
xuhaipeng <xuhaipeng@youbanban.com>
2017-04-24 21:56:24 +0800

    theme

---
+++ www/js/controllers/theme.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/4/22.
+ */
+angular.module('starter.controllers')
+.controller('ThemeCtrl' , function ($scope,$rootScope) {
+  $scope.themes = [
+    {color : '337ab7'},
+    {color : '5cb85c'},
+    {color : '5bc0de'},
+    {color : 'f0ad4e'},
+    {color : 'd9534f'},
+    {color : '696969'},
+    {color : '000000'},
+    {color : 'B8860B'},
+    {color : '8B4513'},
+    {color : '00FF7F'}
+  ];
+
+  var $css = $('.style');
+  $scope.changeTheme = function (color) {
+    console.log(color);
+    $css.attr('href' , 'css/style-'+color+'.css');
+  }
+})
