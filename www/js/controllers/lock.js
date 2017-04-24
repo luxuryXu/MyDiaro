f72f8cb938d63ad2bc07fbdb15d9992f0be33500
xuhaipeng <xuhaipeng@youbanban.com>
2017-04-24 17:03:30 +0800

    lock

--- www/js/controllers/lock.js
+++ www/js/controllers/lock.js
@@ -2,6 +2,18 @@
  * Created by 30613 on 2017/4/17.
  */
 angular.module('starter.controllers')
-.controller('LockCtrl' , function($scope){
+.controller('LockCtrl' , function($scope,$ionicModal,toolsService){
+  $scope.privacy = false;
+  //首先获取当前用户的信息 privacy privacyCode
 
+  $scope.changePrivacy = function(privacy){
+    //修改当前用户的privacy属性
+    if(privacy){
+      toolsService.modal('privacy.html')
+          .then(function (modal) {
+            $scope.loginModal = modal;
+            modal.show();
+      });
+    }
+  }
 });
