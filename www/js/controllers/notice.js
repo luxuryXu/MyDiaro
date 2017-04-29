67b74f1fa67ff50405fccd392417a1a8ee4d7775
xuhaipeng <xuhaipeng@youbanban.com>
2017-04-29 17:54:17 +0800

    notice

---
+++ www/js/controllers/notice.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/4/29.
+ */
+angular.module('starter.controllers')
+.controller('NoticeCtrl' , function($scope){
+  $scope.notices = [
+    {id:1 , title:'MyDiary1.0发布',date:'1288323623006',content:'\n\nMyDiary1.0发布啦！！！'}
+  ];
+});
