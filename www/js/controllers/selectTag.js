445f54f6cc5f2379b05a32cf85cb9262f6aaf0f0
xuhaipeng <xuhaipeng@youbanban.com>
2017-04-30 13:28:07 +0800

    select

---
+++ www/js/controllers/selectTag.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/4/30.
+ */
+angular.module('starter.controllers')
+  .controller('SelectTagCtrl' , function($scope){
+
+  });
--- www/js/controllers/write.js
+++ www/js/controllers/write.js
@@ -4,23 +4,44 @@
 angular.module('starter.controllers')
 .controller('WriteCtrl' , function($scope,$ionicModal,$rootScope){
   $scope.selectType = function(){
-    $ionicModal.fromTemplateUrl('templates/add-type.html',{
+    $ionicModal.fromTemplateUrl('templates/select-type.html',{
       scope:$scope.$new(),
       animation:'slide-in-up'
     })
       .then(function(modal){
-        $scope.addTypeModal = modal;
+        $scope.selectTypeModal = modal;
         modal.show();
       });
-    // $ionicModal.fromTemplateUrl('templates/add-type.html',{
-    //   scope:$scope.$new(),
-    //   animation:'slide-in-up'
-    // })
-    //   .then(function(modal){
-    //     $scope.addTypeModal = modal;
-    //     modal.show();
-    //   });
   }
+  $scope.selectTag = function(){
+    $ionicModal.fromTemplateUrl('templates/select-tag.html',{
+      scope:$scope.$new(),
+      animation:'slide-in-up'
+    })
+      .then(function(modal){
+        $scope.selectTagModal = modal;
+        modal.show();
+      });
+  }
+  $scope.selectLocation = function(){
+    $ionicModal.fromTemplateUrl('templates/select-location.html',{
+      scope:$scope.$new(),
+      animation:'slide-in-up'
+    })
+      .then(function(modal){
+        $scope.selectLocationModal = modal;
+        modal.show();
+      });
+  }
+
+  $scope.$on('closeSelect' , function(){
+    if($scope.selectTypeModal)
+      $scope.selectTypeModal.hide();
+    if($scope.selectTagModal)
+      $scope.selectTagModal.hide();
+    if($scope.selectLocationModal)
+      $scope.selectLocationModal.hide();
+  })
 
   $rootScope.$on('close' , function(){
     if($scope.addTypeModal){
