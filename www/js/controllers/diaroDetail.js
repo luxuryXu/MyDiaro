dca6abbfeda6682a9303ba47d3a53551a34dfa43
xuhaipeng <xuhaipeng@youbanban.com>
2017-05-04 11:29:55 +0800

    diaro-detail

--- www/js/controllers/diaroDetail.js
+++ www/js/controllers/diaroDetail.js
@@ -2,9 +2,12 @@
  * Created by 30613 on 2017/3/27.
  */
 angular.module('starter.controllers')
-  .controller('DiaroCtrl' , function($scope){
-    $scope.time = '2017-03-25 16:13 周一';
-    $scope.collections = 7;
-    $scope.eyeNum = 5;
-    $scope.text = '呵呵呵呵呵呵呵或或或或或或或或呵呵呵呵呵呵呵或或或或或或或或或或或或或或坎坎坷坷扩啥核辐射的防护';
+  .controller('DiaroCtrl' , function($scope,$stateParams,$http,$ionicLoading){
+    $scope.user = JSON.parse(localStorage.user);
+    $http.get('/default/diary/info/' + $stateParams.id)
+      .then(function (res) {
+        $scope.diary = res.data;
+      },function (err) {
+        $ionicLoading.show({template:err.data.message,duration:1000});
+      });
   });
