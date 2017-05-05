5268dd6213d2da52d970254697158eee21db8968
xuhaipeng <xuhaipeng@youbanban.com>
2017-05-05 08:54:03 +0800

    my diary

--- www/js/directives/myDiary.js
+++ www/js/directives/myDiary.js
@@ -9,6 +9,39 @@ angular.module('starter.directives')
       ngModel : '='
     },
     templateUrl : 'templates/myDiary.html',
+    controller:function ($scope) {
+      var now = new Date($scope.ngModel.time);
+      var week = now.getDay();
+      $scope.date = checkTime(now.getDate());
+      $scope.specific = checkTime(now.getHours()) + ':' + checkTime(now.getMinutes());
+      switch (week){
+        case 0:
+          week = '周日';
+          break;
+        case 1:
+          week = '周一';
+          break;
+        case 2:
+          week = '周二';
+          break;
+        case 3:
+          week = '周三';
+          break;
+        case 4:
+          week = '周四';
+          break;
+        case 5:
+          week = '周五';
+          break;
+        case 6:
+          week = '周六';
+          break;
+      }
+      $scope.week = week;
+      function checkTime(n) {
+        return n>10?n:'0'+n;
+      }
+    },
     replace : true
   }
 });
